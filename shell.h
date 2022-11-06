b57a2e170cfef277c0694405bbddd80b7201ea38
vikmokut <vikmokut@gmail.com>
2022-11-06 15:38:28 +0000

    Program files

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*System available Dependencies*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+
+
+#endif /*SHELL_H*/
